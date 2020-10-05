e0911c6b6faeb3ababdf7dff65c8feb2cc22d280
冷斌 <xykxyk2008@163.com>
2020-10-05 23:57:39 +0800

    修改 sh 脚本

--- server.sh
+++ server.sh
@@ -2,11 +2,6 @@
 
 pidFile="runtime/hyperf.pid"
 
-if [ -f "$pidFile" ]; then
-  kill -15 `cat $pidFile`
-fi
-
-rm -rf runtime/container
-php bin/hyperf.php start
-
 php bin/hyperf.php gen:error-code
+
+#php bin/hyperf.php start
