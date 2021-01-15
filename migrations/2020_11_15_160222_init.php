e4cab415c36e75d1a115900a3e6b3d78fe65ca01
冷斌 <xykxyk2008@163.com>
2021-01-16 00:59:49 +0800

    修复 迁移文件

--- migrations/2020_11_15_160222_init.php
+++ migrations/2020_11_15_160222_init.php
@@ -4,7 +4,7 @@ use Hyperf\Database\Schema\Schema;
 use Hyperf\Database\Schema\Blueprint;
 use Hyperf\Database\Migrations\Migration;
 
-class CreateAdminTable extends Migration
+class Init extends Migration
 {
     /**
      * Run the migrations.
