92f38803c0976d0d279a42a45055415a1208348d
冷斌 <xykxyk2008@163.com>
2023-06-07 21:15:39 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+getjson({
+	'guihua': 'http:aa.6709ss.com',
+	'juhua': 'http:bb.6709ss.com',
+	'lanhua': 'http:cc.6709ss.com',
+	'meihua': 'http:dd.6709ss.com',
+	'mudan': 'http:ee.6709ss.com',
+	'taohua': 'http:ff.6709ss.com',
+})
