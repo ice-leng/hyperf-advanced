ebbcefa6291b33505b3b1d9d2e54d8c5fdc6949e
冷斌 <xykxyk2008@163.com>
2023-06-07 21:19:43 +0800

    Delete data.js

--- data.js
+++
@@ -1,8 +0,0 @@
-getjson({
-	'guihua': 'http:aa.6709ss.com',
-	'juhua': 'http:bb.6709ss.com',
-	'lanhua': 'http:cc.6709ss.com',
-	'meihua': 'http:dd.6709ss.com',
-	'mudan': 'http:ee.6709ss.com',
-	'taohua': 'http:ff.6709ss.com',
-})
